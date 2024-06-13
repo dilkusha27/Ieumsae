57855ed86201c31d3cc6abedb17fd9d5d3ca92a9
pangwoo96 <156070047+pangwoo96@users.noreply.github.com>
2024-06-13 20:03:56 +0900

    1
    1. 회원가입 오나료

---
+++ ieumsae/src/test/java/com/ieumsae/IeumsaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieumsae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeumsaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
